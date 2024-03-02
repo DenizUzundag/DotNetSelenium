bb3d890d5749f76ec6c6e29740b8aa83789f7d7c
Deniz Uzundağ <uzundagdeniz@gmail.com>
2024-03-02 21:56:48 +0300

    Proje dosyası ekle.

---
+++ DotNetSelenium/Tests/DataDrivenTesting.cs
@@ -0,0 +1,127 @@
+﻿using DotNetSelenium.Model;
+using DotNetSelenium.Pages;
+using FluentAssertions;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Text.Json;
+
+namespace DotNetSelenium.Tests
+{
+    public class DataDrivenTesting
+    {
+        private IWebDriver _driver;
+  
+
+        [SetUp]
+        public void SetUp()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            _driver.Navigate().GoToUrl("http://eaapp.somee.com/");
+            _driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        [Category("ddt")]
+        [TestCaseSource(nameof(Login))]
+        public void TestWithPOM(LoginModel loginModel)
+        {
+            //POM Initalization
+            LoginPage loginPage = new LoginPage(_driver);
+
+            //Act
+            loginPage.ClickLogin();
+            loginPage.Login(loginModel.Username, loginModel.Password);
+
+            //Assert
+            var getLoggedIn = loginPage.IsLoggedIn();
+            Assert.IsTrue(getLoggedIn.employeeDetails && getLoggedIn.manageUsers);
+        }
+
+
+
+
+        [Test]
+        [Category("ddt")]
+        [TestCaseSource(nameof(Login))]
+        public void TestWithPOMUsingFluentAssertion(LoginModel loginModel)
+        {
+            // POM initalization
+            //Arrange
+            LoginPage loginPage = new LoginPage(_driver);
+
+            //Act
+            loginPage.ClickLogin();
+            loginPage.Login(loginModel.Username, loginModel.Password);
+
+            //Assert
+            var getLoggedIn = loginPage.IsLoggedIn();
+            getLoggedIn.employeeDetails.Should().BeTrue();
+            getLoggedIn.manageUsers.Should().BeTrue();
+        }
+
+
+
+
+        [Test]
+        [Category("ddt")]
+        public void TestWithPOMWithJsonData()
+        {
+            string jsonFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "login.json");
+            var jsonString = File.ReadAllText(jsonFilePath);
+
+            var loginModel = JsonSerializer.Deserialize<LoginModel>(jsonString);
+
+            // POM initalization
+            LoginPage loginPage = new LoginPage(_driver);
+
+            //Act
+            loginPage.ClickLogin();
+
+            loginPage.Login(loginModel.Username, loginModel.Password);
+            //Assert
+            var getLoggedIn = loginPage.IsLoggedIn();
+            Assert.IsTrue(getLoggedIn.employeeDetails && getLoggedIn.manageUsers);
+        }
+
+        public static IEnumerable<LoginModel> Login()
+        {
+            yield return new LoginModel()
+            {
+                Username = "admin",
+                Password = "password"
+            };
+          
+        }
+        public static IEnumerable<LoginModel> LoginJsonDataSource()
+        {
+            string jsonFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "login.json");
+            var jsonString = File.ReadAllText(jsonFilePath);
+
+            var loginModel = JsonSerializer.Deserialize<List<LoginModel>>(jsonString);
+
+            foreach (var loginData in loginModel)
+            {
+                yield return loginData;
+            }
+        }
+        private void ReadJsonFile()
+        {
+
+            string jsonFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "login.json");
+            var jsonString = File.ReadAllText(jsonFilePath);
+
+            var loginModel = JsonSerializer.Deserialize<LoginModel>(jsonString);
+
+            Console.WriteLine($"UserName: {loginModel.Username} Password: {loginModel.Password}");
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            // _driver nesnesini kapatma veya temizleme işlemleri burada yapılmalıdır.
+            _driver.Dispose();
+        }
+    }
+}
---
+++ DotNetSelenium/Tests/NUnitTestDemo.cs
@@ -0,0 +1,109 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using DotNetSelenium.Driver;
+using DotNetSelenium.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace DotNetSelenium.Tests
+{
+    [TestFixture("admin","password",DriverType.Firefox)]
+  
+    public class NunitTestDemo
+    {
+        private IWebDriver _driver;
+        private readonly string userName;
+        private readonly string password;
+        private readonly DriverType driverType;
+        private ExtentReports _extentReports;
+        private ExtentTest _extentTest;
+        private ExtentTest _testNode;
+
+        public NunitTestDemo(string userName, string password, DriverType driverType)
+        {
+            this.userName = userName;
+            this.password = password;
+            this.driverType = driverType;
+
+        }
+        [SetUp] 
+        public void SetUp()
+        {
+            SetupExtentReports();
+            _driver = GetDriverType(driverType);
+            _testNode= _extentTest.CreateNode("Setup and TearDown Info").Pass("Browser Launched");
+            _driver.Navigate().GoToUrl("http://eaapp.somee.com/");
+            _driver.Manage().Window.Maximize();
+         
+        }
+        private IWebDriver GetDriverType(DriverType driverType)
+        {
+            return driverType switch
+            {
+                DriverType.Chrome => new ChromeDriver(),
+                DriverType.Firefox => new FirefoxDriver(),
+                _ =>_driver
+
+            };
+            //switch (driverType)
+            //{
+            //    case DriverType.Chrome:
+            //        _driver = new ChromeDriver();
+            //        break;
+            //    case DriverType.Firefox:
+            //        _driver = new FirefoxDriver();
+            //        break;
+            //}
+
+            //return _driver;
+        }
+        private void SetupExtentReports()
+        {
+            _extentReports = new ExtentReports();
+            var spark = new ExtentSparkReporter("TestReport.html");
+            _extentReports.AttachReporter(spark);
+            _extentReports.AddSystemInfo("OS", "Windows 10");
+            _extentReports.AddSystemInfo("Browser", driverType.ToString());
+            _extentTest = _extentReports.CreateTest("Login Test with POM").Log(Status.Info,"Extent Report Initialize");
+            _extentReports.Flush();
+
+        }
+
+        [Test]
+        [Category("smoke")]
+        public void TestWithPOM()
+        {
+            //POM Initalization
+            LoginPage loginPage = new LoginPage(_driver);
+            loginPage.ClickLogin();
+            _extentTest.Log(Status.Pass, "Click Login");
+            loginPage.Login(userName,password);
+            _extentTest.Log(Status.Pass, "Username and Password entered with login happened");
+            var getLoggedIn = loginPage.IsLoggedIn();
+            Assert.IsTrue(getLoggedIn.employeeDetails && getLoggedIn.manageUsers);
+            _extentTest.Log(Status.Pass, "Assertion succesful");
+
+        }
+
+        [Test]
+        [TestCase("chrome","30")]
+        public void TestBrowserVersion(string browser,string version)
+        {
+            Console.WriteLine($"The browser is {browser} with version {version}");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // _driver nesnesini kapatma veya temizleme işlemleri burada yapılmalıdır.
+            _driver.Dispose();
+            _testNode.Pass("Browser quit");
+            _extentReports.Flush();
+        
+        }
+
+
+    }
+}
+
